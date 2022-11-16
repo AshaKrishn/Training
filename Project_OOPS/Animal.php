fd03a7eac9ac4227ed7b13af90e103f41457445f
AshaKrishn <asha.krishna@2hatslogic.com>
2022-11-16 16:08:05 +0530

    Changed to PSR-2 stds

--- Project_OOPS/Animal.php
+++ Project_OOPS/Animal.php
@@ -1,105 +1,112 @@
 <?php
 
-class Animal {
+class Animal
+{
     public $type;
     public $colour;
-    
     private static $legs = 4;
     public static $tails = 1;
 
-    public static function Description() {
+    public static function description()
+    {
         echo "I have ".self::$legs." legs. <br>";
     }
-
-    public function Welcome(){
+    public function welcome()
+    {
         echo "Hi I am a $this->colour $this->type...!! <br>";
-        
-    }
-    
-   
+    }    
 }
 
-class Carnivorous extends Animal {
+class Carnivorous extends Animal 
+{
     public $name;
-    public static function Description(){
+    public static function description()
+    {
         echo "I have two eyes.<br>";
     }
-    public function Intro() {
-        $this->Welcome();
-        //self::Description();
-        parent::Description();
+    public function intro()
+    {
+        $this->welcome();
+        self::description();
+        parent::description();
         echo "My name is $this->name and I eat meat !! <br>";
     }
-
 }
 
-class Herbivorous extends Animal {
+class Herbivorous extends Animal
+{
     public $name;
-    protected function Intro() {
-        $this->Welcome();
+    protected function intro()
+    {
+        $this->welcome();
         echo "My name is $this->name and I eat plants !! <br>";
     }
 }
 
-class Omnivorous extends Animal {
+class Omnivorous extends Animal
+{
     public $name;
-    protected function Intro() {
-        $this->Welcome();
+    protected function intro()
+    {
+        $this->welcome();
         echo "I have ".self::$tails." tail.<br>";
         echo "My name is $this->name and I eat both plants and meat !! <br>";
     }
-    
 }
 
-Final class Dog extends Omnivorous {
-    private function Sound() {
+final class Dog extends Omnivorous
+{
+    private function sound()
+    {
         echo "Woof Woof..!! <br>";
     }
-    private function Habitat(){
+    private function habitat()
+    {
         echo "I live in a Kennel <br>";
     }
-
-    public function AboutMe(){
-        $this->Intro();
-        $this->Sound();
-        $this->Habitat();
+    public function aboutMe()
+    {
+        $this->intro();
+        $this->sound();
+        $this->habitat();
     }
-    
 }
 
-Final class Cow extends Herbivorous {
-    private function Sound() {
+final class Cow extends Herbivorous
+{
+    private function sound()
+    {
         echo "Moo Moo Moo !! <br>";
     }
-    private function Habitat(){
+    private function habitat()
+    {
         echo "I live in a Farm <br>";
     }
-    public function AboutMe(){
-        $this->Intro();
-        $this->Sound();
+    public function aboutMe()
+    {
+        $this->intro();
+        $this->sound();
     }
-    
 }
 
 $carnivorous = new Carnivorous();
 $carnivorous->type = "Tiger";
 $carnivorous->colour = "Brown";
 $carnivorous->name = "Diego";
-$carnivorous->Intro();
+$carnivorous->intro();
 echo "=====================================<br>";
 
 $cow = new Cow();
 $cow->type = "Cow";
 $cow->colour = "Grey";
 $cow->name = "Kunjumol";
-$cow->AboutMe();
+$cow->aboutMe();
 echo "=====================================<br>";
 
 $dog = new Dog();
 $dog->type = "Dog";
 $dog->colour = "Black";
 $dog->name = "Mia";
-$dog->AboutMe();
+$dog->aboutMe();
 echo "=====================================<br>";
 
-?>
