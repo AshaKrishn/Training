c843af5283ea3474466b385b143f9471241d9c9a
AshaKrishn <asha.krishna@2hatslogic.com>
2022-12-30 18:11:54 +0530

    my blog

---
+++ MyBlog/resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+    <div class="text-center p-4 copyright">
+        Copyright &copy; your website 2022
+    </div>
+</footer>
---
+++ MyBlog/resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<title>My Blog</title>
+<nav class="navbar navbar-expand-md">
+    <div class="container-lg">
+        <h1><a href="/">Blogs</a></h1>
+        <div class="navbar-nav ms-auto"><a href='/register'> Register </a></div>
+    </div>
+</nav>
